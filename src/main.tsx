4ef2ff06031450b9d696cf457d8409a737a1860a
okuda1506 <okuda1506@gmail.com>
2023-05-29 12:29:10 +0900

    PWA化

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
+import { registerSW } from 'virtual:pwa-register';
+
 const root = createRoot(document.getElementById('root') as Element);
 
 root.render(
@@ -9,3 +11,5 @@ root.render(
 		<App />
 	</StrictMode>
 );
+
+registerSW();
